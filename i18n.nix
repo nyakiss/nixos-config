519cb747d772a32695dfe62da71dc44c0c497b80
nyakiss <nyakiss@yandex.ru>
2021-05-31 03:15:53 +0700

    create

---
+++ i18n.nix
@@ -0,0 +1,22 @@
+{ config, lib, pkgs, ... }:
+
+with lib; {
+  # Select internationalization properties.
+  console =  {
+    earlySetup = mkDefault true;
+    font = mkDefault "ter-v16n";
+    packages = [ pkgs.terminus_font ];
+    useXkbConfig = mkDefault true;
+  };
+
+  i18n.defaultLocale = mkDefault "ru_RU.UTF-8";
+
+  services.xserver = {
+    xkbModel = "pc105";
+    layout = "us,ru";
+    xkbOptions = "grp:caps_toggle,grp_led:caps";
+  };
+
+  # Set your time zone.
+  time.timeZone = mkDefault "Asia/Novokuznetsk";
+}
